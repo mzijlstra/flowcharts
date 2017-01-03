72ee99e4b9bb54346957983c7c2bb757ec67fa09
ProfZijlstra <mzijlstra@mum.edu>
2017-01-03 20:29:43 +0100

    refactored turtle graphics code

--- res/js/utils.js
+++ res/js/utils.js
@@ -5,7 +5,258 @@
 var __turtle_win = false;
 
 (function () {
-    var createCanvasPopup = function (width, height) {
+    // private variable to count graphics windows
+    var windowCount = 0;
+
+    // private helper function
+    var toRad = function (deg) {
+        return deg / 180 * Math.PI;
+    };
+
+    // functions related to the turtle itself
+    var drawTurtle = function () {
+        var ctx = this.display[0].getContext("2d");
+        ctx.clearRect(0, 0, 32, 32);
+
+        // draw body
+        ctx.fillStyle = "#882";
+        var path = new Path2D();
+        path.moveTo(26, 17);
+        path.arc(16, 17, 10, 0, Math.PI * 2);
+        ctx.fill(path);
+        ctx.stroke(path);
+
+        // draw limbs
+        ctx.fillStyle = "#4A4";
+        path = new Path2D();
+        path.moveTo(16, 5);
+        path.arc(16, 5, 4, 0, Math.PI * 2);
+        path.moveTo(8, 12);
+        path.arc(8, 12, 4, 0, Math.PI * 2);
+        path.moveTo(24, 12);
+        path.arc(24, 12, 4, 0, Math.PI * 2);
+        path.moveTo(11, 25);
+        path.arc(11, 25, 4, 0, Math.PI * 2);
+        path.moveTo(21, 25);
+        path.arc(21, 25, 4, 0, Math.PI * 2);
+        ctx.fill(path);
+
+        // draw face
+        ctx.strokeStyle = "#4A4";
+        path = new Path2D();
+        path.moveTo(14, 1);
+        path.lineTo(18, 1);
+        ctx.stroke(path);
+        ctx.strokeStyle = "purple";
+        ctx.lineWidth = 2;
+        path = new Path2D();
+        path.moveTo(12, 2);
+        path.lineTo(20, 2);
+        path.moveTo(20, 4);
+        path.lineTo(22, 4);
+        ctx.stroke(path);
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "black";
+        path = new Path2D();
+        path.moveTo(13, 2.5);
+        path.lineTo(15, 2.5);
+        path.moveTo(17, 2.5);
+        path.lineTo(19, 2.5);
+        path.moveTo(13, 5.5);
+        path.lineTo(19, 5.5);
+        path.moveTo(16, 5.5);
+        path.lineTo(16, 6.5);
+        path.moveTo(13, 5.5);
+        path.lineTo(13, 4.5);
+        path.moveTo(19, 5.5);
+        path.lineTo(19, 4.5);
+        ctx.stroke(path);
+    };
+    var drawPenDown = function () {
+        var ctx = this.display[0].getContext("2d");
+        var path = new Path2D();
+        ctx.fillStyle = "black";
+        path.moveTo(30, 28);
+        path.lineTo(28, 30);
+        path.lineTo(9, 6);
+        path.lineTo(11, 4);
+        ctx.fill(path);
+        path = new Path2D();
+        path.moveTo(30, 30);
+        path.lineTo(28, 30);
+        path.lineTo(30, 28);
+        ctx.fillStyle = this.penIsColor;
+        ctx.fill(path);
+    };
+    var drawPenUp = function () {
+        var ctx = this.display[0].getContext("2d");
+        ctx.strokeStyle = "rgb(0,0,0,0.5)";
+        ctx.lineWidth = 1;
+        var path = new Path2D();
+        path.moveTo(28, 30);
+        path.lineTo(32, 30);
+        path.moveTo(30, 28);
+        path.lineTo(30, 32);
+        ctx.stroke(path);
+        ctx.fillStyle = "black";
+        path = new Path2D();
+        path.moveTo(0, 12);
+        path.lineTo(32, 12);
+        path.lineTo(32, 15);
+        path.lineTo(0, 15);
+        ctx.fill(path);
+    };
+
+    var toString = function () {
+        return "[object Turtle]";
+    };
+    var forward = function (amount) {
+        var dx = Math.cos(toRad(this.deg)) * amount;
+        var dy = Math.sin(toRad(this.deg)) * amount;
+        this.display.css({
+            "top": this.y + dy - 30,
+            "left": this.x + dx - 30
+        });
+        if (this.penIsDown) {
+            var path = new Path2D();
+            path.moveTo(this.x, this.y);
+            path.lineTo(this.x + dx, this.y + dy);
+            this.parentCtx.stroke(path);
+        }
+        this.x += dx;
+        this.y += dy;
+        return this;
+    };
+    var rotate = function (degree) {
+        this.deg += degree;
+        this.deg = this.deg % 360;
+        this.display.css({
+            "transform": "rotate(" + this.deg + "deg)"
+        });
+        return this;
+    };
+    var rotateRight = function (degree) {
+        rotate(-degree);
+    };
+    var penUp = function () {
+        this.penIsDown = false;
+        this.drawTurtle();
+        this.drawPenUp();
+        return this;
+    };
+    var penDown = function () {
+        this.penIsDown = true;
+        this.drawTurtle();
+        this.drawPenDown();
+        return this;
+    };
+    var penColor = function (color) {
+        this.penIscolor = color;
+        this.drawTurtle();
+        if (this.penIsDown) {
+            this.drawPenDown();
+        } else {
+            this.drawPenUp();
+        }
+        this.parentCtx.strokeStyle = color;
+        return this;
+    };
+    var penWidth = function (width) {
+        this.lineWidth = width;
+        this.parentCtx.lineWidth = width;
+        return this;
+    };
+    var show = function () {
+        this.display.show();
+        return this;
+    };
+    var hide = function () {
+        this.display.hide();
+        return this;
+    };
+    var moveTo = function (nx, ny) {
+        this.display.css({
+            "top": ny - 16,
+            "left": nx - 16
+        });
+        if (this.penIsDown) {
+            var path = new Path2D();
+            path.moveTo(this.x, this.y);
+            path.lineTo(nx, ny);
+            this.parentCtx.stroke(path);
+        }
+        this.x = nx;
+        this.y = ny;
+        return this;
+    };
+    var rotateTo = function (degree) {
+        this.deg = degree % 360;
+        this.display.css({
+            "transform": "rotate(" + (this.deg + 90) + "deg)"
+        });
+        return this;
+    };
+
+    // private function to create a turtle on a graphics window
+    var createTurtle = function () {
+        // Create a turtle display
+        var display = $("<canvas width='32' height='32'>");
+        display.css({
+            "position": "absolute",
+            "top": $(this.document).height() / 2 - 30,
+            "left": $(this.document).width() / 2 - 30,
+            "transform-origin": "30px 30px"
+        });
+        $(this.document.body).append(display);
+
+        var turtle = {
+            // variables
+            "display": display,
+            "parentCtx": $(this.document).find("#canvas")[0].getContext('2d'),
+            "deg": 0,
+            "penIsColor": "black",
+            "penIsDown": true,
+            "lineWidth": 2,
+            "x": $(this.document).width() / 2,
+            "y": $(this.document).height() / 2,
+            // system functions
+            "drawTurtle": drawTurtle,
+            "drawPenUp": drawPenUp,
+            "drawPenDown": drawPenDown,
+            "toString": toString,
+            // user functions
+            "forward": forward,
+            "rotate": rotate,
+            "left": rotate,
+            "right": rotateRight,
+            "penUp": penUp,
+            "penDown": penDown,
+            "penColor": penColor,
+            "penWidth": penWidth,
+            "show": show,
+            "hide": hide,
+            "moveTo": moveTo,
+            "rotateTo": rotateTo
+        };
+
+        turtle.parentCtx.lineWidth = turtle.lineWidth;
+        turtle.parentCtx.strokeStyle = turtle.penIsColor;
+
+        turtle.drawTurtle();
+        turtle.drawPenDown();
+
+        return turtle;
+    };
+
+    /**
+     * public function to create a graphics window
+     * 
+     * @param {int} width
+     * @param {int} height
+     * @returns {object|Window.TGWindow.win|Window|utils_L7.createCanvasPopup.win|window.TGWindow.win}
+     */
+    window.GfxWindow = function (width, height) {
+        // parameter defaults (if not provided)
         if (!width || typeof (width) !== 'number') {
             width = 400;
         }
@@ -19,283 +270,34 @@ var __turtle_win = false;
             w = window.top;
         }
 
+        // create the window object
         var parent_x = $(document).width();
-
         var features = "top=250,left=" + (parent_x - width) + ",width=" +
                 (width + 1) + ",height=" + (height + 4) + ",menubar=0";
 
-        var win = window.open(undefined, "popup", features);
+        var win = window.open(undefined, "popup" + windowCount, features);
         if (!win) {
             throw "Unable to create window -- is the popup blocked?";
         }
+        // TODO check if popup was still open from refresh
+        // so that we can close old windows before opening new ones!
+        
+        
+        windowCount++;
+        win.document.title = "Graphics Window " + windowCount;
+
+        // create the window content
         var body = $(win.document.body);
         body.css({"margin": "0"});
-        var canvas = $("<canvas id='canvas' width='" + width + "' height='" + 
+        var canvas = $("<canvas id='canvas' width='" + width + "' height='" +
                 height + "'>");
         body.append(canvas);
+
+        // add the window public methods
+        win.createTurtle = createTurtle;
         win.toString = function () {
             return "[object CanvasWindow]";
         };
-        return win;
-    };
-
-    var createTurtle = function () {
-        // Create a turtle display
-        var display = $("<canvas width='32' height='32'>");
-        display.css({
-            "position": "absolute",
-            "top": $(this.document).height() / 2 - 30,
-            "left": $(this.document).width() / 2 - 30,
-            "transform-origin": "30px 30px"
-        });
-        $(this.document.body).append(display);
-
-        // private variables and functions for object
-        var toRad = function (deg) {
-            return deg / 180 * Math.PI;
-        };
-        // TODO make penColor and lineWidth public properties!
-        var pen_color = "black";
-        var ctx = $(this.document).find("#canvas")[0].getContext('2d');
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = pen_color;
-        var penDown = true;
-        var deg = 0;
-        var x = $(this.document).width() / 2;
-        var y = $(this.document).height() / 2;
-
-        var drawTurtle = function () {
-            var ctx = display[0].getContext("2d");
-            ctx.clearRect(0, 0, 32, 32);
-
-            // draw body
-            ctx.fillStyle = "#882";
-            var path = new Path2D();
-            path.moveTo(26, 17);
-            path.arc(16, 17, 10, 0, Math.PI * 2);
-            ctx.fill(path);
-            ctx.stroke(path);
-
-            // draw limbs
-            ctx.fillStyle = "#4A4";
-            path = new Path2D();
-            path.moveTo(16, 5);
-            path.arc(16, 5, 4, 0, Math.PI * 2);
-            path.moveTo(8, 12);
-            path.arc(8, 12, 4, 0, Math.PI * 2);
-            path.moveTo(24, 12);
-            path.arc(24, 12, 4, 0, Math.PI * 2);
-            path.moveTo(11, 25);
-            path.arc(11, 25, 4, 0, Math.PI * 2);
-            path.moveTo(21, 25);
-            path.arc(21, 25, 4, 0, Math.PI * 2);
-            ctx.fill(path);
-
-            // draw face
-            ctx.strokeStyle = "#4A4";
-            path = new Path2D();
-            path.moveTo(14, 1);
-            path.lineTo(18, 1);
-            ctx.stroke(path);
-            ctx.strokeStyle = "purple";
-            ctx.lineWidth = 2;
-            path = new Path2D();
-            path.moveTo(12, 2);
-            path.lineTo(20, 2);
-            path.moveTo(20, 4);
-            path.lineTo(22, 4);
-            ctx.stroke(path);
-            ctx.lineWidth = 1;
-            ctx.strokeStyle = "black";
-            path = new Path2D();
-            path.moveTo(13, 2.5);
-            path.lineTo(15, 2.5);
-            path.moveTo(17, 2.5);
-            path.lineTo(19, 2.5);
-            path.moveTo(13, 5.5);
-            path.lineTo(19, 5.5);
-            path.moveTo(16, 5.5);
-            path.lineTo(16, 6.5);
-            path.moveTo(13, 5.5);
-            path.lineTo(13, 4.5);
-            path.moveTo(19, 5.5);
-            path.lineTo(19, 4.5);
-            ctx.stroke(path);
-        };
-        var drawPenDown = function () {
-            var ctx = display[0].getContext("2d");
-            var path = new Path2D();
-            ctx.fillStyle = "black";
-            path.moveTo(30, 28);
-            path.lineTo(28, 30);
-            path.lineTo(9, 6);
-            path.lineTo(11, 4);
-            ctx.fill(path);
-            path = new Path2D();
-            path.moveTo(30, 30);
-            path.lineTo(28, 30);
-            path.lineTo(30, 28);
-            ctx.fillStyle = pen_color;
-            ctx.fill(path);
-        };
-        var drawPenUp = function () {
-            var ctx = display[0].getContext("2d");
-            ctx.strokeStyle = "rgb(0,0,0,0.5)";
-            ctx.lineWidth = 1;
-            var path = new Path2D();
-            path.moveTo(28, 30);
-            path.lineTo(32, 30);
-            path.moveTo(30, 28);
-            path.lineTo(30, 32);
-            ctx.stroke(path);
-            ctx.fillStyle = "black";
-            path = new Path2D();
-            path.moveTo(0, 12);
-            path.lineTo(32, 12);
-            path.lineTo(32, 15);
-            path.lineTo(0, 15);
-            ctx.fill(path);
-        };
-        drawTurtle();
-        drawPenDown();
-
-        var turtle = {
-            'toString': function () {
-                return "[object Turtle]";
-            },
-            'forward': function (amount) {
-                var dx = Math.cos(toRad(deg)) * amount;
-                var dy = Math.sin(toRad(deg)) * amount;
-                display.css({
-                    "top": y + dy - 30,
-                    "left": x + dx - 30
-                });
-                if (penDown) {
-                    var path = new Path2D();
-                    path.moveTo(x, y);
-                    path.lineTo(x + dx, y + dy);
-                    ctx.stroke(path);
-                }
-                x += dx;
-                y += dy;
-                return this;
-            },
-            'rotate': function (degree) {
-                deg += degree;
-                deg = deg % 360;
-                display.css({
-                    "transform": "rotate(" + deg + "deg)"
-                });
-                return this;
-            },
-            'penUp': function () {
-                penDown = false;
-                drawTurtle();
-                drawPenUp();
-                return this;
-            },
-            'penDown': function () {
-                penDown = true;
-                drawTurtle();
-                drawPenDown();
-                return this;
-            },
-            'penColor': function (color) {
-                pen_color = color;
-                drawTurtle();
-                if (penDown) {
-                    drawPenDown();
-                } else {
-                    drawPenUp();
-                }
-                ctx.strokeStyle = color;
-                return this;
-            },
-            'penWidth': function (width) {
-                ctx.lineWidth = width;
-                return this;
-            },
-            'show': function () {
-                display.show();
-                return this;
-            },
-            'hide': function () {
-                display.hide();
-                return this;
-            },
-            'moveTo': function (nx, ny) {
-                display.css({
-                    "top": ny - 16,
-                    "left": nx - 16
-                });
-                if (penDown) {
-                    path = new Path2D();
-                    path.moveTo(x, y);
-                    path.lineTo(nx, ny);
-                    ctx.stroke(path);
-                }
-                x = nx;
-                y = ny;
-                return this;
-            },
-            'rotateTo': function (degree) {
-                deg = degree % 360;
-                display.css({
-                    "transform": "rotate(" + (deg + 90) + "deg)"
-                });
-                return this;
-            }
-        };
-        return turtle;
-    };
-
-    /**
-     * Global constructor function for creating a turtle graphics window
-     * @param {int} width
-     * @param {int} height
-     * @returns {object} reference to the newly created window
-     */
-    window.TGWindow = function (width, height) {
-        var win = createCanvasPopup(width, height);
-        win.document.title = "Turtle Graphics";
-        win.createTurtle = createTurtle;
-        return win;
-    };
-    
-    /**
-     * Single function to create a window and a turtle
-     * 
-     * @param {int} width
-     * @param {int} height
-     * @returns {object} reference to the created turtle
-     */
-    window.TurtleGFX = function(width, height) {
-        // global variable!
-        if (__turtle_win) {
-            __turtle_win.close();
-        }
-        __turtle_win = window.TGWindow(width, height);
-        return __turtle_win.createTurtle();
-    };
-    
-    
-    /**
-     * Global constructor funfction for an image manipulation window
-     * 
-     * @param {type} width
-     * @param {type} height
-     * @param {type} url
-     * @returns {Window|Window.ImgWindow.win}
-     */
-    window.ImgWindow = function (width, height, url) {
-        var win = createCanvasPopup(width, height);
-        $(win).load(function () {
-            var img = new Image();
-            img.src = url;
-            img.onload = function () {
-
-            };
-        });
         return win;
     };
 }());
